37a6737ff86676f1aa676e5260c68172b68cb99e
Nicole Gervasoni <nicole.gervasoni@gmail.com>
2024-04-18 08:54:43 +0000

    Add check for coverage from afl-whatup

--- harness.c
+++ harness.c
@@ -3,7 +3,7 @@
 #include "library.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-    char *data = (char *)Data;
-    lib_echo(data, Size);
+    // Step 3: call lib_echo with the input data
+    lib_echo((char *)Data, Size);
     return 0;
 }
